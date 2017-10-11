393f62fa3584cfbb33a676fdfd0fe24e44a5b693
leo <ivashchenko.leo@gmail.com>
2017-10-11 19:55:10 +0300

    add homework for week 3 part 3 task 2

---
+++ nn.py
@@ -0,0 +1,235 @@
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d as p3
+import numpy as np
+import random
+import time
+import math
+
+from functools import partial
+from ipywidgets import interact, RadioButtons, IntSlider, FloatSlider, Dropdown, BoundedFloatText
+from numpy.linalg import norm
+
+random.seed(42) # начальное состояние генератора случайных чисел, чтобы можно было воспроизводить результаты.
+
+## Определим разные полезные функции
+
+def sigmoid(x):
+    """сигмоидальная функция, работает и с числами, и с векторами (поэлементно)"""
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_prime(x):
+    """производная сигмоидальной функции, работает и с числами, и с векторами (поэлементно)"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def J_quadratic(neuron, X, y):
+    """
+    Оценивает значение квадратичной целевой функции.
+    Всё как в лекции, никаких хитростей.
+
+    neuron - нейрон, у которого есть метод vectorized_forward_pass, предсказывающий значения на выборке X
+    X - матрица входных активаций (n, m)
+    y - вектор правильных ответов (n, 1)
+        
+    Возвращает значение J (число)
+    """
+    
+    assert y.shape[1] == 1, 'Incorrect y shape'
+    
+    return 0.5 * np.mean((neuron.vectorized_forward_pass(X) - y) ** 2)
+
+def J_quadratic_derivative(y, y_hat):
+    """
+    Вычисляет вектор частных производных целевой функции по каждому из предсказаний.
+    y_hat - вертикальный вектор предсказаний,
+    y - вертикальный вектор правильных ответов,
+    
+    В данном случае функция смехотворно простая, но если мы захотим поэкспериментировать 
+    с целевыми функциями - полезно вынести эти вычисления в отдельный этап.
+    
+    Возвращает вектор значений производной целевой функции для каждого примера отдельно.
+    """
+    
+    assert y_hat.shape == y.shape and y_hat.shape[1] == 1, 'Incorrect shapes'
+    
+    return (y_hat - y) / len(y)
+    
+def compute_grad_analytically(neuron, X, y, J_prime=J_quadratic_derivative):
+    """
+    Аналитическая производная целевой функции
+    neuron - объект класса Neuron
+    X - вертикальная матрица входов формы (n, m), на которой считается сумма квадратов отклонений
+    y - правильные ответы для примеров из матрицы X
+    J_prime - функция, считающая производные целевой функции по ответам
+    
+    Возвращает вектор размера (m, 1)
+    """
+    
+    # Вычисляем активации
+    # z - вектор результатов сумматорной функции нейрона на разных примерах
+    
+    z = neuron.summatory(X)
+    y_hat = neuron.activation(z)
+
+    # Вычисляем нужные нам частные производные
+    dy_dyhat = J_prime(y, y_hat)
+    dyhat_dz = neuron.activation_function_derivative(z)
+    
+    # осознайте эту строчку:
+    dz_dw = X
+
+    # а главное, эту:
+    grad = ((dy_dyhat * dyhat_dz).T).dot(dz_dw)
+    
+    # можно было написать в два этапа. Осознайте, почему получается одно и то же
+    # grad_matrix = dy_dyhat * dyhat_dz * dz_dw
+    # grad = np.sum(, axis=0)
+    
+    # Сделаем из горизонтального вектора вертикальный
+    grad = grad.T
+    
+    return grad
+
+class Neuron:
+    
+    def __init__(self, weights, activation_function=sigmoid, activation_function_derivative=sigmoid_prime):
+        """
+        weights - вертикальный вектор весов нейрона формы (m, 1), weights[0][0] - смещение
+        activation_function - активационная функция нейрона, сигмоидальная функция по умолчанию
+        activation_function_derivative - производная активационной функции нейрона
+        """
+        
+        assert weights.shape[1] == 1, "Incorrect weight shape"
+        
+        self.w = weights
+        self.activation_function = activation_function
+        self.activation_function_derivative = activation_function_derivative
+        
+    def forward_pass(self, single_input):
+        """
+        активационная функция логистического нейрона
+        single_input - вектор входов формы (m, 1), 
+        первый элемент вектора single_input - единица (если вы хотите учитывать смещение)
+        """
+        
+        result = 0
+        for i in range(self.w.size):
+            result += float(self.w[i] * single_input[i])
+        return self.activation_function(result)
+    
+    def summatory(self, input_matrix):
+        """
+        Вычисляет результат сумматорной функции для каждого примера из input_matrix. 
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вектор значений сумматорной функции размера (n, 1).
+        """
+        return input_matrix.dot(self.w)
+    
+    def activation(self, summatory_activation):
+        """
+        Вычисляет для каждого примера результат активационной функции,
+        получив на вход вектор значений сумматорной функций
+        summatory_activation - вектор размера (n, 1), 
+        где summatory_activation[i] - значение суммматорной функции для i-го примера.
+        Возвращает вектор размера (n, 1), содержащий в i-й строке 
+        значение активационной функции для i-го примера.
+        """
+        return self.activation_function(summatory_activation)
+        
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Векторизованная активационная функция логистического нейрона.
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных.
+        Возвращает вертикальный вектор размера (n, 1) с выходными активациями нейрона
+        (элементы вектора - float)
+        """
+        return self.activation(self.summatory(input_matrix))
+        
+    def SGD(self, X, y, batch_size, learning_rate=0.1, eps=1e-6, max_steps=200):
+        """
+        Внешний цикл алгоритма градиентного спуска.
+        X - матрица входных активаций (n, m)
+        y - вектор правильных ответов (n, 1)
+        
+        learning_rate - константа скорости обучения
+        batch_size - размер батча, на основании которого 
+        рассчитывается градиент и совершается один шаг алгоритма
+        
+        eps - критерий остановки номер один: если разница между значением целевой функции 
+        до и после обновления весов меньше eps - алгоритм останавливается. 
+        Вторым вариантом была бы проверка размера градиента, а не изменение функции,
+        что будет работать лучше - неочевидно. В заданиях используйте первый подход.
+        
+        max_steps - критерий остановки номер два: если количество обновлений весов 
+        достигло max_steps, то алгоритм останавливается
+        
+        Метод возвращает 1, если отработал первый критерий остановки (спуск сошёлся) 
+        и 0, если второй (спуск не достиг минимума за отведённое время).
+        """
+        indexes_all = range(0, X.shape[0])
+        steps = 0
+        
+        while True:
+            indexes_random = np.random.choice(indexes_all, batch_size, replace=False)
+            batch_x = X[indexes_random]
+            batch_y = y[indexes_random]
+            is_collided = self.update_mini_batch(batch_x, batch_y, learning_rate, eps)
+            steps += 1
+            
+            if is_collided or steps == max_steps:
+                return int(is_collided)
+        
+    
+    def update_mini_batch(self, X, y, learning_rate, eps):
+        """
+        X - матрица размера (batch_size, m)
+        y - вектор правильных ответов размера (batch_size, 1)
+        learning_rate - константа скорости обучения
+        eps - критерий остановки номер один: если разница между значением целевой функции 
+        до и после обновления весов меньше eps - алгоритм останавливается. 
+        
+        Рассчитывает градиент (не забывайте использовать подготовленные заранее внешние функции) 
+        и обновляет веса нейрона. Если ошибка изменилась меньше, чем на eps - возвращаем 1, 
+        иначе возвращаем 0.
+        """
+        predictions = self.vectorized_forward_pass(X)
+        target_function_before = J_quadratic(self, X, y)
+        nabla = compute_grad_analytically(self, X, y)
+        self.w -= learning_rate * nabla
+        predictions = self.vectorized_forward_pass(X)
+        target_function_after = J_quadratic(self, X, y)
+        
+        return math.fabs(target_function_before - target_function_after) < eps
+
+inputs = np.array([[1, 0, 1, 2]])
+w21 = np.array([0, 8, 7, 8])
+w22 = np.array([0, 10, 10, 9])
+w31 = np.array([0, 10, 9])
+
+w21 = np.reshape(w21, (len(w21), 1))
+w22 = np.reshape(w22, (len(w22), 1))
+w31 = np.reshape(w31, (len(w31), 1))
+
+y = np.ndarray([1])
+y = y.reshape((len(y), 1))
+
+n21 = Neuron(w21, activation_function=sigmoid, activation_function_derivative=sigmoid_prime)
+n22 = Neuron(w22, activation_function=sigmoid, activation_function_derivative=sigmoid_prime)
+
+n31 = Neuron(w31, activation_function=sigmoid, activation_function_derivative=sigmoid_prime)
+
+r2 = np.array([[1, n21.vectorized_forward_pass(inputs)[0][0], n22.vectorized_forward_pass(inputs)[0][0]]])
+
+r3 = n31.vectorized_forward_pass(r2)[0][0]
+er3 = compute_grad_analytically(n31, r2, y) * sigmoid_prime(n31.summatory(r2))
+er3 = er3.mean(axis=0)
+
+er21 = er3.dot(w31.T) * sigmoid_prime(n21.summatory(inputs))
+er22 = er3.dot(w31.T) * sigmoid_prime(n22.summatory(inputs))
+er21 = er21.mean(axis=1)
+er22 = er22.mean(axis=1)
+
+result1 = 2 * er22
+result2 = 2 * er21
+print(result1, result2)
